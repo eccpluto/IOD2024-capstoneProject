d830ac7e3783d58ea6f953ebc1cc545204cb2652
eccpluto <alleyes.payday591@passmail.net>
2024-07-14 17:04:20 +1200

    dotenv, mongoose, and basic server with mongodb access setup

--- backend/ideo/index.js
+++ backend/ideo/index.js
@@ -1,1 +1,22 @@
-console.log("hello there");
+const express = require("express");
+const app = express();
+
+// access .env variables
+require("dotenv").config();
+
+// connect to database (optionally depends on .env and hence dotenv.config() preceding this)
+require("./dbConnect");
+
+app.use(express.json());
+
+// TODO pull in routes
+app.get("/", (req, res) => {
+    console.log("At server root.");
+    res.send({result: 200, data: "at root of server"});
+})
+
+// typically, as .env would not be provided with source code, we provide a default PORT string
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
