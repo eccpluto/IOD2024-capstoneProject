0c028822b6938240efc5cff0e1fbb472a867ad9b
eccpluto <alleyes.payday591@passmail.net>
2024-07-14 19:01:55 +1200

    http routes and database CRUD ops for user collection done

--- backend/ideo/controllers/userController.js
+++ backend/ideo/controllers/userController.js
@@ -6,25 +6,44 @@ const userModel = models.user;
 const createUser = (req, res) => {
     console.log(`Creating user: ${req.body}`);
     new userModel(req.body).save()
+        .then(data => res.send({ result: 200, data: data }))
+        .catch(err => res.send({ result: 500, error: err.message }));
+};
+
+const getUsers = (req, res) => {
+    console.log("Getting all users.");
+    userModel.find({})
+        .then(data => res.send({ result: 200, data: data }))
+        .catch(err => res.send({ result: 500, error: err.message }));
+};
+
+const getUserById = (req, res) => {
+    console.log(`Getting user id: ${req.params.id}.`);
+    userModel.findById(req.params.id)
+        .then(data => res.send({ result: 200, data: data }))
+        .catch(err => res.send({ result: 500, error: err.message }));
+};
+
+const updateUserById = (req, res) => {
+    console.log(`Updating user id: ${req.params.id}.`);
+    // passing { new: true } will return the updated record
+    userModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(data => res.send({result: 200, data: data}))
         .catch(err => res.send({result: 500, error: err.message}));
 };
 
-const getUsers = (req, res) => {
-
-};
-
-const updateUser = (req, res) => {
-
-};
-
-const deleteUser = (req, res) => {
-
+const deleteUserById = (req, res) => {
+    console.log(`Deleting user id: ${req.params.id}.`);
+    // passing { new: true } will return the updated record
+    userModel.findByIdAndDelete(req.params.id, req.body, { new: true })
+        .then(data => res.send({result: 200, data: data}))
+        .catch(err => res.send({result: 500, error: err.message}));
 }
 
 module.exports = {
     createUser,
     getUsers,
-    updateUser,
-    deleteUser,
+    getUserById,
+    updateUserById,
+    deleteUserById,
 }
