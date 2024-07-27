fcf56beb97d669182afe59c1b66158c01a95efdf
eccpluto <alleyes.payday591@passmail.net>
2024-07-27 17:16:05 +1200

    fixed port number

--- backend/ideo/index.js
+++ backend/ideo/index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 
 // typically, as .env would not be provided with source code, we provide a default PORT string
-// process.env.PORT || 8080
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
